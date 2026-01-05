ab49cf8f7a6c607b0fb17bb887617c193ca216cd
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-01-05 18:49:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../styles/App.css"; // Keeping your existing style import
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 
 const App = () => {
   return (
-    <div>
+    // Requirement: Set up BrowserRouter
+    <BrowserRouter>
+      <div>
         {/* Do not remove the main div */}
-    </div>
-  )
+        <Routes>
+          {/* Requirement: Home route at '/' */}
+          <Route path="/" element={<ItemList />} />
+          
+          {/* Requirement: Dynamic route at '/items/{id}' */}
+          <Route path="/items/:id" element={<ItemDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
